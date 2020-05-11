38d4853323dc3935614a179ed8984b95ffb04226
Smit Shah <smir2k14@gmail.com>
2020-05-11 12:18:35 +0530

    Added an initial README

---
+++ README.md
@@ -0,0 +1,6 @@
+### Repo made for people who have reinstalled ubuntu due to some major **** ups and want to get some of their previous settings back
+
+
+# Contributions are extremely encouraged. 
+
+# Cheers!
---
+++ ubuntu-utilities.sh
@@ -0,0 +1,15 @@
+sudo apt update && sudo apt upgrade
+sudo apt-get install python3-pip
+sudo apt-get install vim
+sudo apt-get install git
+sudo apt-get install zsh
+
+#Oh-my-zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+./install.sh
+
+#VScode
+sudo snap install code --classic
+
+#GNOME Tweak Tool for customization
+sudo apt-get install gnome-tweak-tool
